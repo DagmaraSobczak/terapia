916f83c33563f7c30e90aa883f296c36993af78b
DagmaraSobczak <daga.sobczak@gmail.com>
2024-04-13 00:49:18 +0200

    vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import mdx from "@mdx-js/rollup";
 
 export default defineConfig({
-  plugins: [react()],
-  base: "/terapia/",
-  server: {
-    mimeTypes: {
-      "application/javascript": ["js", "jsx"],
-    },
-  },
+  plugins: [
+    { enforce: "pre", ...mdx() },
+    react({ include: /\.(mdx|js|jsx|ts|tsx)$/ }),
+  ],
 });
